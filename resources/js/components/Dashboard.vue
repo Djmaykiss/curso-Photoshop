9336d6d13c55940e4cff52e39671582ee3225aed
Michael <102557016+Djmaykiss@users.noreply.github.com>
2024-01-08 18:09:42 -0500

    Update Dashboard.vue

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -5,23 +5,24 @@
             <div>
                 <h2>Bienvenido al Curso de Photoshop Online</h2>
                 <!-- Puedes usar un video o una imagen para la introducción -->
-<iframe width="1350" height="480" src="https://www.youtube.com/embed/4-VMuLs99Ss" title="Curso Photoshop - Capitulo 0, Introducción para Principiantes" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>            </div>
+                <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F61553959814822%2Fvideos%2F1103205307523615%2F&show_text=false&width=560&t=0" width="auto" height="auto" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+            </div>
         </section>
 
         <section id="cursos">
             <!-- Lista de todos los cursos -->
             <h2>Tus Cursos</h2>
             <ul>
-                <li><a href="/./Cursos/Curso 1.php" >Lección 1: Introducción </a></li>
-                <li><a href="/./Cursos/Curso 2.php" >Lección 2: Herramientas Básicas de Photoshop</a></li>
-                <li><a href="/./Cursos/Curso 3.php" >Lección 3: Capas y Máscaras</a></li>
-                <li><a href="/./Cursos/Curso 4.php" >Lección 4: Filtros y Efectos</a></li>
-                <li><a href="/./Cursos/Curso 5.php" >Lección 5: Técnicas Avanzadas de Retoque</a></li>
-                <li><a href="/./Cursos/Curso 6.php" >Lección 6: Creación de Gráficos y Diseño Visual</a></li>
-                <li><a href="/./Cursos/Curso 7.php" >Lección 7: Trabajo con Texturas y Tipografías</a></li>
-                <li><a href="/./Cursos/Curso 8.php" >Lección 8: Principios de Diseño de Interfaces</a></li>
-                <li><a href="/./Cursos/Curso 9.php" >Lección 9: #</a></li>
-                <li><a href="/./Cursos/Curso 10.php" >Lección 10: #</a></li>
+                <li><a href="/cursos/curso1" >Lección 1: Introducción a Photoshop</a></li>
+                <li><a href="/cursos/curso1" >Lección 2: Herramientas Básicas de Photoshop</a></li>
+                <li><a href="/cursos/curso1" >Lección 3: Capas y Máscaras</a></li>
+                <li><a href="/cursos/curso1" >Lección 4: Filtros y Efectos</a></li>
+                <li><a href="/cursos/curso1" >Lección 5: Técnicas Avanzadas de Retoque</a></li>
+                <li><a href="/cursos/curso1" >Lección 6: Creación de Gráficos y Diseño Visual</a></li>
+                <li><a href="/cursos/curso1" >Lección 7: Trabajo con Texturas y Tipografías</a></li>
+                <li><a href="/cursos/curso1" >Lección 8: Principios de Diseño de Interfaces</a></li>
+                <li><a href="/cursos/curso1" >Lección 9: #</a></li>
+                <li><a href="/cursos/curso1" >Lección 10: #</a></li>
                 <!-- Agrega más lecciones según sea necesario -->
             </ul>
         </section>
